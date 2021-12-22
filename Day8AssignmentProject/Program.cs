8fb51c63c343db36a33abfad6a3c2dbd6b59ba56
hkvijay54 <hkvijay54@gmail.com>
2021-12-22 18:08:11 +0530

    Emp present or not

---
+++ Day8AssignmentProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+// check employee pesent or not
+class Program
+{
+    public void empChek()
+    {
+        //constants
+        int IS_FULL_TIME = 1;
+        Random random = new Random();
+
+        int empCheck = random.Next(0, 2);
+        if (empCheck == IS_FULL_TIME)
+        {
+            Console.WriteLine("Employee is Present = " + empCheck);
+        }
+        else
+        {
+            Console.WriteLine("Employee is absent = " + empCheck);
+        }
+    }
+    static void Main(string[] args)
+    {
+        Program emp = new Program();
+        
+        emp.empChek();
+    }
+}
